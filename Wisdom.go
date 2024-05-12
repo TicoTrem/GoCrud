b387daf5f8901537900128e06489ce0126f31f86
Tyson Tremblay <tremblay0995@saskpolytech.ca>
2024-05-12 11:45:02 -0600

    Got some of the CRUD functionality working

--- Wisdom.go
+++ Wisdom.go
@@ -1,21 +1,57 @@
-package gocrud
+package main
 
 import (
-	"database/sql"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func WisdomHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("mysql", "tyson:password@/WisdomDB")
+type Wisdom struct {
+	Source   string
+	Wisdom   string
+	Datetime string
+}
+
+func WisdomGetter(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := db.Query("SELECT * FROM wisdom")
 	if err != nil {
 		panic(err)
 	}
 
-	db.SetConnMaxLifetime(time.Minute * 3)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
+	var id int
+	var source string
+	var wisdom string
+	var date string
+	for rows.Next() {
+		err = rows.Scan(&id, &source, &wisdom, &date)
+		if err != nil {
+			panic(err)
+		}
+		print("Source is: ", source, "\nWisdom is: ", wisdom, "\n", "Date is: ", date, "\n")
+	}
 
-	rows, err := db.Query("SELECT * FROM Wisdom")
+}
 
+func WisdomPoster(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT * FROM wisdom")
+	if err != nil {
+		panic(err)
+	}
+
+	// decode json
+	var json_data Wisdom
+	// maps the json data to the Wisdom
+	json.NewDecoder(r.Body).Decode(&json_data)
+
+	query := fmt.Sprintf("INSERT INTO wisdom (source, wisdom, datetime) VALUES (\"%v\", \"%v\", \"%v\")", json_data.Source, json_data.Wisdom, time.Now().Format("2006-01-02 15:04:05"))
+	print(query)
+	// we don't use := here because we are not initializing any new variables
+	_, err = db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+
+	print(rows)
 }
--- main.go
+++ main.go
@@ -1,29 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"time"
 
-	// the http handler I am using
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
-// this crud will allow you to add useful tips
-func main() {
-	print("test")
+var db *sql.DB
 
-	// This will hold the data of our data
-	type Wisdom struct {
-		Source string
-		Wisdom string
-		date   time.Time
+type Wisdom struct {
+	Source   string
+	Wisdom   string
+	Datetime string
+}
+
+func main() {
+
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Println("Current date and time:", currentTime)
+
+	fmt.Println()
+
+	var err error
+	db, err = sql.Open("mysql", "root:password@tcp(127.0.0.1)/WisdomDB")
+	if err != nil {
+		panic(err)
 	}
+	defer db.Close()
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
 
 	router := mux.NewRouter()
-
 	router.HandleFunc("/", HomeHandler)
-	router.HandleFunc("Wisdom", WisdomGetter).Methods("GET")
+	router.HandleFunc("/Wisdom", WisdomPoster).Methods("POST")
+	router.HandleFunc("/Wisdom", WisdomGetter).Methods("GET")
+
+	// Start the server after all routes are configured
+	http.ListenAndServe(":3000", router)
+	print("finished")
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
